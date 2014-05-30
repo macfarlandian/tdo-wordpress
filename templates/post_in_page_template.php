ea21eb73b34fd1e1a0614cea1c17ecb0510bf4e8
Ian MacFarland <ianmacfarland@ischool.berkeley.edu>
2014-05-29 23:07:35 -0700

    add custom post in page template
    (for ivy cat posts in page plugin)

---
+++ templates/post_in_page_template.php
@@ -0,0 +1,9 @@
+<!-- NOTE: This must be copied to the active theme directory in order to be used -->
+
+<!-- Start of Post Wrap -->
+<div class="ivycat-post">
+	<!-- This is the output of the post TITLE -->
+	<h3 class=""><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+
+</div>
+<!-- // End of Post Wrap -->
